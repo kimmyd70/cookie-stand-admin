9037e567d08d5fcc122505f82523eebbbb9dde98
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-04-13 17:02:23 -0700

    styling complete

--- cookie-stand-admin/pages/index.js
+++ cookie-stand-admin/pages/index.js
@@ -2,28 +2,46 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="min-h-screen box-border bg-green-50">
+    <div class="min-h-screen box-border bg-green-50">
       <Head>
         <title>Cookie Stand Admin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className = "p-3 text-xl bg-green-500">
+      <header class = "text-xl bg-green-500">
         <h1>Cookie Stand Admin</h1>
       </header>
 
-      <main className="text-base text-center">
-        <h2 className="m-4">Create Cookie Stand</h2>
+      <main class="text-center">
         {/* <form onSubmit = {createCookieStandHandler} name = "formData" className= "flex-row mx-48 min-w-min p-4 my-5 rounded-md bg-green-300"> */}
-        <form  name = "formData" className= "flex-row mx-48 min-w-min p-4 my-5 rounded-md bg-green-300">
+        <form  name = "formData" class= "flex-row mx-28 p-4 my-5 rounded-md bg-green-300">
+          <h2 class="mb-5">Create Cookie Stand</h2>
 
-          <input name = "create" className="">
-          </input>
-          <button className ="px-2 py-1 by-gray-500">Create</button>
+          <div class="flex mb-5">
+            <label for="location" class="pr-4 pt-2 text-xs">Location</label>
+            <input name = "location" class="flex-1 bg-gray-200"></input>
+          </div>
+
+          <div class="flex items-center gap-x-4 mt-5 text-xs">
+              <div class="flex-1">
+                <label for="minCustomers" class="">Maximum Customers Per Hour</label>
+                <input name="minCustomers" class="w-full"></input>
+              </div>
+              <div class="flex-1">
+                <label for="maxCustomers" class="">Minimum Customers Per Hour</label>
+                <input name="maxCustomers" class="w-full"></input>
+              </div>
+              <div class="flex-1">
+                <label for="avgCookies" class="">Average Cookies Per Sale</label>
+                <input name="avgCookies" class="w-full"></input>
+              </div>
+            <button class ="flex-1 pt-4 pb-4 text-xs bg-green-500">Create</button>
+          </div>
         </form>
+        <h3 class="">Report Table Coming Soon ...</h3>
       </main>
 
-      <footer className="p-3 text-sm bg-green-500" >
+      <footer class="p-3 text-sm bg-green-500" >
         <p>&copy; 2021</p>
       </footer>
     </div>
