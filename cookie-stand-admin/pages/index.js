5dcfa92bbeb7cc8e3e52805dbeda81526116f26f
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-04-20 14:58:39 -0700

    minor styling before break

--- cookie-stand-admin/pages/index.js
+++ cookie-stand-admin/pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header class = "flex justify-between text-xl bg-green-500 p-4">
+      <header class = "flex justify-between text-3xl bg-green-500 p-4">
         <h1 class = "pl-4">Cookie Stand Admin</h1>
         {/* <link href ="/overview"> */}
           <button class = "bg-gray-50 p-2 border rounded mr-4 text-sm items-center" href = "/overview"><a>Overview</a></button>
@@ -35,11 +35,11 @@ export default function Home() {
           <h2 class="mb-5">Create Cookie Stand</h2>
 
           <div class="flex mb-5">
-            <label for="location" class="pr-3 text-sm">Location</label>
-            <input name = "location" class="flex-1 pl-2 text-sm bg-gray-200"></input>
+            <label for="location" class="pr-3">Location</label>
+            <input name = "location" class="flex-1 pl-2  bg-gray-200"></input>
           </div>
 
-          <div class="flex items-center gap-x-4 mt-5 text-xs">
+          <div class="flex items-center gap-x-4 mt-5 ">
               <div class="flex-1">
                 <label for="minCustomers" class="">Maximum Customers Per Hour</label>
                 <input type ="number" step="0.1" name="minCustomers" class="w-full"></input>
@@ -52,7 +52,7 @@ export default function Home() {
                 <label for="avgCookies" class="">Average Cookies Per Sale</label>
                 <input type ="number" step="0.1" name="avgCookies" class="w-full"></input>
               </div>
-            <button class ="flex-1 rounded-md pt-3 pb-3 text-xs bg-green-500">Create</button>
+            <button class ="flex-1 rounded-md pt-3 pb-3  bg-green-500">Create</button>
           </div>
         </form>
         {/* <div><h3 class="my-5 text-gray-400">Report Table Coming Soon ...</h3>
